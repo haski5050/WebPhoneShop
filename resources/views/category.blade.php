0612040d150887c6d6784752f1f5383257dd1d9e
haski5050 <haski5050@gmail.com>
2021-02-09 13:54:51 +0200

    'categoriesStyle'

--- resources/views/category.blade.php
+++ resources/views/category.blade.php
@@ -4,18 +4,16 @@
 @endsection
 @section('content')
     <div class="container">
+        <tr>
         @foreach($categories as $el)
+            <td>
             <form method="post" action="{{route('categorySelect')}}">
                 @csrf
-                <div class="col p-1">
-                    <div class="row-cols-3">
                 <button class="btn btn-outline-success" type="submit">{{$el->Name}}</button>
-                    </div>
-                    <div class="col">
                 <input name="categoryId" value="{{$el->ID}}" hidden class="form-control">
-                    </div>
-                </div>
             </form>
+            </td>
         @endforeach
+        </tr>
 </div>
 @endsection
